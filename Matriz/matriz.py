acc47f564aa8eb7e5b967fb9cfd079ba5810c773
julyanpatricio <julyan.patricio@gmail.com>
2021-08-05 13:04:16 -0300

    agregado de docTest

--- Matriz/matriz.py
+++ Matriz/matriz.py
@@ -11,10 +11,36 @@ import numpy as np
 ''' Crear una matriz de 5x5 randomizada con números enteros, encontrar secuencia de 4 números consecutivos horizontal o vertical y si se encuentra mostrar la posición inicial y final '''
 
 def busqueda_4_consecutivos(matriz, nRecorrido = 0):
-    for x in range(matriz.shape[0]):
+    '''
+    pre-condicion: dic debe ser una lista de diccionarios que contangan como claves las palabras 'id' y 'edad' y sus valores sean numeros enteros
+    
+    --- TEST **hacerlo mejor requeriria mayor tiempo para realizar varios try-except** ---
+    >>> busqueda_4_consecutivos([[1,2,3,4,5],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]])
+    {'busqueda': 'horizontal', 'posicion_inicial': (0, 0), 'posicion_final': (0, 3)}
+    
+    >>> busqueda_4_consecutivos([[1,1,1,1,1],[2,1,1,1,1],[3,1,1,1,1],[4,1,1,1,1],[5,1,1,1,1]])
+    {'busqueda': 'vertical', 'posicion_inicial': (0, 0), 'posicion_final': (3, 0)}
+    
+    >>> busqueda_4_consecutivos([[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]])
+    False
+    
+    Parameters
+    ----------
+    matriz : numpy.ndarray | list
+        Debe ser una matriz cuadrada.
+    nRecorrido : int, optional
+        Es un control interno para realizar la busqueda tanto horizontal como verticalmente. Puede indicarse el valor 1 para realizar la busqueda solo horizonalmente. The default is 0.
+
+    Returns
+    -------
+    dict | bool
+        En caso de encontrar 4 numeros consecutivos, devuelve un diccionario con la direccion en la que se encontro y la posicion inicial y final de los elementos. Caso contrario devuelve el booleano False
+
+    '''
+    for x in range(len(matriz)):
         nConsecutivos = 1
         
-        for y in range(matriz.shape[1] - 1):
+        for y in range(len(matriz) - 1):
             if matriz[x][y]+1 == matriz[x][y+1]:
                 nConsecutivos += 1
                 
@@ -23,25 +49,23 @@ def busqueda_4_consecutivos(matriz, nRecorrido = 0):
                 
             if nConsecutivos == 4:
                 if nRecorrido == 0:
-                    return f"La posicion inicial es ({x},{y-2}) y la final ({x},{y+1})"
+                    return {'busqueda': 'horizontal', 'posicion_inicial': (x, y-2), 'posicion_final':(x, y+1)}
                 
-                return f" en traspose La posicion inicial es ({y-2},{x}) y la final ({y+1},{x})"
+                return {'busqueda': 'vertical', 'posicion_inicial': (y-2, x), 'posicion_final':(y+1, x)}
     
     if nRecorrido == 0:
-        
         return busqueda_4_consecutivos(np.transpose(matriz), 1)
 
-    return 'no existe 4 numeros consecutivos en ninguna direccion'
+    return False
     
         
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    matriz = np.random.randint(4, size=(5, 5))
+    resultado = busqueda_4_consecutivos(matriz)
+    if(resultado):
+        print (f"Se encontraron 4 numeros consecutivos en forma {resultado['busqueda']} desde la posicion {resultado['posicion_inicial']} hasta la posicion {resultado['posicion_final']}")
+    else:
+        print('no existe 4 numeros consecutivos en ninguna direccion')
 
-matriz = np.random.randint(4, size=(5, 5))
-print(busqueda_4_consecutivos(matriz))
-
-matriz2 = np.transpose(np.array([
-    [1,2,3,4,5],
-    [1,1,1,1,1],
-    [1,1,1,1,1],
-    [1,1,1,1,1],
-    [1,1,1,1,1]]))
-print(busqueda_4_consecutivos(matriz2))
